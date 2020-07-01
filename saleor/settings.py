b0d989851132d240555f64e6a4683b4298cb7085
Farhan Saeed <farhansaeed27@gmail.com>
2020-07-01 15:48:03 +0500

    add new packages

--- saleor/settings.py
+++ saleor/settings.py
@@ -56,7 +56,7 @@ CACHES = {"default": django_cache_url.config()}
 
 DATABASES = {
     "default": dj_database_url.config(
-        default="postgres://saleor:saleor@localhost:5432/erocery", conn_max_age=600
+        default="postgres://saleor:saleor@localhost:5432/saleor", conn_max_age=600
     )
 }
 
